fcb21e2a64368e15d1498408a9542609c5606443
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2016-09-07 15:52:59 +0200

    Working webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const BUILD_DIR = path.resolve(__dirname, 'src/client/public')
+const APP_DIR = path.resolve(__dirname, 'src/client/app')
+
+module.exports = {
+  entry: [
+    // 'webpack/hot/only-dev-server',
+    // './js/app.js'
+    APP_DIR + '/index.jsx'
+  ],
+
+  output: {
+    // path: __dirname + '/build',
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      // { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
+      { test: /\.js?$/, loader: 'babel', exclude: /node_modules/ },
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: 'style!css' }
+    ]
+  },
+
+  plugins: [
+    new webpack.NoErrorsPlugin()
+  ]
+}
