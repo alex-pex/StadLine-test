070c3792080150c9c0d9a002015a7048cdab4ede
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2016-09-07 20:51:23 +0200

    Created Router to handle requests

---
+++ src/client/app/Components/Issue/Issue.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react"
+import Conversation from "../Conversation/Conversation.jsx"
+
+export default class Issue extends Component {
+  render() {
+    return (
+      <div>
+        <Conversation />
+      </div>
+    )
+  }
+}
